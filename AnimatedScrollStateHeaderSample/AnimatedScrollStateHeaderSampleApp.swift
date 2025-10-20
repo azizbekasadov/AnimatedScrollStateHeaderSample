4fb06662bd3cb4168d07c97cccc1f9966b8e9b98
Azizbek Asadov <azizbek.asadov.work@gmail.com>
2025-10-21 00:15:52 +0200

    Initial Commit

---
+++ AnimatedScrollStateHeaderSample/AnimatedScrollStateHeaderSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedScrollStateHeaderSampleApp.swift
+//  AnimatedScrollStateHeaderSample
+//
+//  Created by Azizbek Asadov on 21.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedScrollStateHeaderSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
